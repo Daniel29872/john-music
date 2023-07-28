a7f43a9e0551108103fd9dc2cce55d0604f04ecd
Daniel29872 <arnholtdaniel@gmail.com>
2023-07-28 17:19:13 -0400

    feat: added logic for play and skip commands

--- src/bot.py
+++ src/bot.py
@@ -6,11 +6,9 @@ from discord.commands.context import ApplicationContext
 from discord.ext import commands
 
 
-intents = discord.Intents.default()
-intents.message_content = True
 bot = commands.Bot()
-
-queue = list()
+vc: discord.VoiceClient
+queue = []
 
 with open(f"{os.getcwd()}/token.yaml") as file:
     token = yaml.safe_load(file)["token"]
@@ -19,29 +17,35 @@ with open(f"{os.getcwd()}/token.yaml") as file:
 @bot.slash_command(
     name="play", description="Adds a song to the queue.", guild_ids=bot.guilds
 )
-async def play(ctx: ApplicationContext, args):
-    await ctx.respond("placeholder")
+async def play(ctx: ApplicationContext, arg: str):
+    global vc
+    if not ctx.author.voice:
+        await ctx.respond("member not in voice channel", delete_after=5)
+        return
 
+    if vc and vc.is_connected():
+        await vc.disconnect()
 
-@bot.slash_command(
-    name="pause", description="Pauses the current song.", guild_ids=bot.guilds
-)
-async def pause(ctx: ApplicationContext, args):
-    await ctx.respond("placeholder")
-
-
-@bot.slash_command(
-    name="resume", description="Resumes the current song.", guild_ids=bot.guilds
-)
-async def resume(ctx: ApplicationContext, args):
-    await ctx.respond("placeholder")
+    vc = await ctx.author.voice.channel.connect()  # TODO: add song to queue
+    await ctx.respond("playing song", delete_after=5)
 
 
 @bot.slash_command(
     name="skip", description="Skips the current song.", guild_ids=bot.guilds
 )
-async def skip(ctx: ApplicationContext, args):
-    await ctx.respond("placeholder")
+async def skip(ctx: ApplicationContext, arg: str = None):
+    global vc
+
+    if not ctx.author.voice:
+        await ctx.respond("member not in voice channel", delete_after=5)
+    if not vc or not vc.is_connected():
+        await ctx.respond("bot not in voice channel", delete_after=5)
+
+    if queue:  # TODO: skip song, download and play next
+        await ctx.respond("skipping current song", delete_after=5)
+    else:
+        await ctx.voice_client.disconnect()
+        await ctx.respond("queue is empty, disconnecting", delete_after=5)
 
 
 @bot.event
